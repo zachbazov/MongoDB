1b69f048f94e07fab835c4f2f160bca308fec8d4
zachbazov <zachbazov@gmail.com>
2023-09-24 13:24:03 +0300

    Implemented `MongoAuthRequestable` contract for handling url auth-related requests.

--- Package.swift
+++ Package.swift
@@ -11,15 +11,10 @@ let package = Package(
             name: "MongoDB",
             targets: ["MongoDB"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/zachbazov/URLAuthentication.git", from: "1.0.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MongoDB", dependencies: [
-                "URLAuthentication"
-            ]),
+            name: "MongoDB"),
     ]
 )
