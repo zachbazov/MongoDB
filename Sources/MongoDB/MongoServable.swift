615c68764eea259f928ebb374ab2c8496b7f296e
zachbazov <zachbazov@gmail.com>
2023-09-29 15:53:59 +0300

    Updated `MongoServable` contract.

--- Sources/MongoDB/MongoServable.swift
+++ Sources/MongoDB/MongoServable.swift
@@ -10,12 +10,7 @@ import URLDataTransfer
 
 public protocol MongoServable {
     
-    associatedtype ServerType: MongoServable
-    associatedtype StoreType: ResponsePersistable
     associatedtype DataTransferType: DataTransferRequestable
     
     var dataTransferService: DataTransferType { get }
-    
-    func serverDidLaunch(_ server: ServerType)
-    func server(_ server: ServerType, reauthenticationFromStore store: StoreType)
 }
