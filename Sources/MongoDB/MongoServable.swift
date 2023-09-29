1348ba1cc890b184ddb548c4fe2945173207f67b
zachbazov <zachbazov@gmail.com>
2023-09-29 15:44:49 +0300

    Moved delegation functionallity into `MongoServable`.

--- Sources/MongoDB/MongoServable.swift
+++ Sources/MongoDB/MongoServable.swift
@@ -5,11 +5,17 @@
 //  Created by Developer on 29/09/2023.
 //
 
+import CodeBureau
 import URLDataTransfer
 
 public protocol MongoServable {
     
+    associatedtype ServerType: MongoServable
+    associatedtype StoreType: ResponsePersistable
     associatedtype DataTransferType: DataTransferRequestable
     
     var dataTransferService: DataTransferType { get }
+    
+    func serverDidLaunch(_ server: ServerType)
+    func server(_ server: ServerType, reauthenticationFromStore store: StoreType)
 }
